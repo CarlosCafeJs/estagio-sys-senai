c25e78662a5683b0e9648967ef476fec734b545a
Carlos Henrique dos Santos <129197782+CarlosCafeJs@users.noreply.github.com>
2024-08-22 15:17:03 +0000

    Adicionando arquivos.

---
+++ estagiosys/src/main/java/com/example/estagioserpro/estagiosys/EstagiosysApplication.java
@@ -0,0 +1,13 @@
+package com.example.estagioserpro.estagiosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstagiosysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstagiosysApplication.class, args);
+	}
+
+}
